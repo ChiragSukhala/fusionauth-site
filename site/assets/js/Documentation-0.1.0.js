66111cb0aaac4831df8fc148acb5be892e805fdd
Daniel DeGroff <daniel@fusionauth.io>
2020-04-24 18:21:52 -0600

    working.

--- site/assets/js/Documentation-0.1.0.js
+++ site/assets/js/Documentation-0.1.0.js
@@ -6,14 +6,24 @@ var FusionAuth = FusionAuth || {};
 FusionAuth.Documentation = function() {
   Prime.Utils.bindAll(this);
 
-  this.scrollTop = Prime.Document.queryById('scroll-top').addEventListener('click', this._handleClick);
-  window.onscroll = this._handleScroll;
+  this.tripHeight = 2000;
+
+  this.scrollTop = Prime.Document.queryById('scroll-top');
+  if (this.scrollTop !== null) {
+    this.scrollTop.addEventListener('click', this._handleClick);
+    window.onscroll = this._handleScroll;
+  }
+
+  // Hide if the page is short so we don't this button high up on the page.
+  if (window.scrollY < this.tripHeight) {
+    this.scrollTop.hide();
+  }
+
   this._replaceBreadcrumbsWithGlyphs();
 }
 
 FusionAuth.Documentation.constructor = FusionAuth.Documentation;
 FusionAuth.Documentation.prototype = {
-
 
   _handleClick: function(event) {
     Prime.Utils.stopEvent(event);
@@ -32,7 +42,8 @@ FusionAuth.Documentation.prototype = {
 
   _handleScroll: function() {
     if (!this.scrolling) {
-      if (window.pageYOffset > 2000) {
+      if (window.pageYOffset > this.tripHeight) {
+        this.scrollTop.show();
         this.scrollTop.addClass('show');
       } else {
         this.scrollTop.removeClass('show')
